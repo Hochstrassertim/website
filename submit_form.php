940289e1b4574d2343cdff68e2ae91fb07e3607c
Hochstrassertim <hochstrassertim15@gmail.com>
2023-12-20 21:18:33 +0100

    Added support for email

--- submit_form.php
+++ submit_form.php
@@ -1,4 +1,12 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Include PHPMailer autoloader
+require 'PHPmailer/src/Exception.php';
+require 'PHPmailer/src/PHPMailer.php';
+require 'PHPmailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $first_name = $_POST["first_name_input"];
     $last_name = $_POST["last_name_input"];
@@ -7,18 +15,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $category = $_POST["category_select"];
     $message = $_POST["message_input"];
 
-    // Your email sending logic here
-    $to = "your@email.com";
-    $subject = "Form Submission - $category";
-    $body = "First Name: $first_name\nLast Name: $last_name\nEmail: $email\nPhone: $phone\nCategory: $category\nMessage: $message";
+    // Initialize PHPMailer
+    $mail = new PHPMailer(true);
 
-    // Use appropriate headers (e.g., content-type and additional headers)
-    mail($to, $subject, $body);
+    try {
+        // Set mailer to use SMTP
+        $mail->isSMTP();
 
-    // You can also include additional logic, such as saving the form data to a database
-    // ...
+        // SMTP server settings
+        $mail->Host       = 'timhoch.ch';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'noreply@timhoch.ch';
+        $mail->Password   = 'Brachi15';
+        $mail->SMTPSecure = 'tls'; // Use 'tls' or 'ssl' depending on your server configuration
+        $mail->Port       = 587;   // Use 587 for TLS, 465 for SSL
 
-    echo "Form submitted successfully!";
+        // Sender and recipient
+        $mail->setFrom($email, 'Sender Name');
+        $mail->addAddress('hochstrassertim15@gmail.com', 'Recipient Name');
+
+        // Email content
+        $mail->Subject = "Form Submission - $category";
+        $mail->Body    = "First Name: $first_name\nLast Name: $last_name\nEmail: $email\nPhone: $phone\nCategory: $category\nMessage: $message";
+
+        // Send the email
+        $mail->send();
+
+        echo "Form submitted successfully!";
+    } catch (Exception $e) {
+        echo "Error submitting form. Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 } else {
     echo "Invalid request method!";
 }
