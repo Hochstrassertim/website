7be7969715a174e3f5397d0adb5b299c16f13ed3
Tim Hochstrasser <hochstrassert@bzz.ch>
2023-12-20 15:56:47 +0100

    Initial commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-?>
-
-<html>
-<form class="support_form" id="support_form">
-    <label for="first_name_input">First name</label>
-    <input class="first_name_input" id="first_name_input" type="text" required>
-</form>
-</html>
