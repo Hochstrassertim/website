0938e04c5310b2e07e3aa01e232961d4139d2379
Hochstrassertim <hochstrassertim15@gmail.com>
2023-12-23 21:26:16 +0100

    Added light and dark mode

--- content/script.js
+++ content/script.js
@@ -7,10 +7,25 @@ function redirectToCorrectMode() {
 }
 
 function redirectToOtherPage(page) {
-    console.log(window.location.href); /*
-    if (document.getElementById("light_dark_mode").value == "light") {
-        window.location.href = page + "?mode=light";
-    } else {
-        window.location.href = page + "?mode=dark";
-    }*/
+    // Get the current URL
+    var currentUrl = window.location.href;
+
+    // Extract the mode parameter from the URL
+    var urlParams = new URLSearchParams(window.location.search);
+    var currentMode = urlParams.get("mode");
+
+    // Set the dropdown selection based on the current mode
+    var dropdown = document.getElementById("light_dark_mode");
+
+    if (currentMode === "light") {
+        dropdown.value = "light";
+    } else if (currentMode === "dark") {
+        dropdown.value = "dark";
+    }
+
+    // Log the current URL to the console (optional)
+    console.log("Current URL:", currentUrl);
+
+    // Redirect to the specified page with the selected mode
+    window.location.href = page + "?mode=" + dropdown.value;
 }
